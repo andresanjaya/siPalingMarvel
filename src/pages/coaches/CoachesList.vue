98542e3e6bfe8bdaa1e3b040bd847a0f7c813012
andresanjaya <andresanjaya2506@gmail.com>
2022-06-21 09:58:37 +0800

    first commit

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+      FILTER
+  </section>
+  <section>
+      LIST OF COACHES
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachDetail from './pages/coaches/CoachDetail'
+import CoachesList from './pages/coaches/CoachesList'
+import CoachRegistration from './pages/coaches/CoachRegistration'
+import ContactCoach from './pages/request/ContactCoach'
+import RequestReceived from './pages/request/RequestReceived'
+import NotFound from './pages/NotFound'
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: 'coaches' },
+        { path: '/coaches', component: CoachesList },
+        { path: '/coaches/:id', component: CoachDetail, children: [
+            { path: '/contact', component: ContactCoach }, // /coaches/c1/contact
+        ] },
+        { path: '/register', component: CoachRegistration },
+        { path: '/request', component: RequestReceived },
+        { path: '/:notFound(.*)', component: NotFound },
+    ]
+})
+
+export default router;
