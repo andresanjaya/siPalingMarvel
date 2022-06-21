c0ab59e29531e88f9e860dba9428e2f40316b8f9
andresanjaya <andresanjaya2506@gmail.com>
2022-06-21 10:10:21 +0800

    Working on the Main Layout & Styling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
+  <the-header></the-header>
   <router-view>
 
   </router-view>
 </template>
 
 <script>
+import TheHeader from './components/layouts/TheHeader.vue'
 export default {
-
+    components: {
+        TheHeader
+    }
 }
 </script>
 
