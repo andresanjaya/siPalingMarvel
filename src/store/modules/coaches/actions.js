599703041fb95828306b4990a8ac933c11c61cfa
andresanjaya <andresanjaya2506@gmail.com>
2022-06-22 15:08:36 +0800

    Fetching Coach Data (GET Http Request)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -22,5 +22,27 @@ export default {
       ...coachData,
       id: userId
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(`https://vue-first-project-19994-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`)
+    const responseData = await response.json()
+
+    if (!response.ok) {
+      //
+    }
+
+    const coaches = []
+
+      for (const key in responseData) {
+        const coach = {
+          firstName: responseData[key].firstName,
+          lastName: responseData[key].lastName,
+          description: responseData[key].description,
+          hourlyRate: responseData[key].hourlyRate,
+          areas: responseData[key].areas
+        }
+        coaches.push(coach)
+      }
+      context.commit('setCoaches', coaches)
   }
 };
