0a65bdbdb6b3ea4b41223573dbfe7c3cb0a3ded8
andresanjaya <andresanjaya2506@gmail.com>
2022-06-24 14:39:53 +0800

    Adding "Auto Login"

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,37 +1,28 @@
 export default {
   async login(context, payload) {
-    const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB9qGQuz4Z1Njmjq8os5y_GFYS5z7MjZm8',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true
-        })
-      }
-    );
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.message || 'Failed to authenticate. Check your login data.'
-      );
-      throw error;
-    }
-
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
-    });
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
+    })
   },
+
   async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    })
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB9qGQuz4Z1Njmjq8os5y_GFYS5z7MjZm8'
+    
+    if (mode === 'signup') {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9qGQuz4Z1Njmjq8os5y_GFYS5z7MjZm8'
+    }
+
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9qGQuz4Z1Njmjq8os5y_GFYS5z7MjZm8',
+      url,
       {
         method: 'POST',
         body: JSON.stringify({
@@ -52,13 +43,29 @@ export default {
       throw error;
     }
 
-    console.log(responseData);
+    localStorage.setItem('token', responseData.idToken)
+    localStorage.setItem('userId', responseData.userId)
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn
     });
   },
+
+  tryLogin(context) {
+    const token = localStorage.getItem('token')
+    const userId = localStorage.getItem('userId')
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null
+      })
+    }
+  },
+
   logout(context) {
     context.commit('setUser', {
       token: null,
